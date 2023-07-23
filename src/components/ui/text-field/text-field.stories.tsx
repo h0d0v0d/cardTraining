0992dbd335009ece43cd573421a655f896fe949d
Anna Krasilnikova <akr6363@mail.ru>
2023-07-23 16:55:28 +0300

    -install storybook

--- src/components/ui/text-field/text-field.stories.tsx
+++ src/components/ui/text-field/text-field.stories.tsx
@@ -1,26 +1,26 @@
-// import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-// import { TextField } from './'
-//
-// const meta = {
-//   title: 'Components/TextField',
-//   component: TextField,
-//   tags: ['autodocs'],
-//   argTypes: {
-//     variant: {
-//       options: ['primary', 'secondary', 'tertiary', 'link'],
-//       control: { type: 'radio' },
-//     },
-//   },
-// } satisfies Meta<typeof Button>
-//
-// export default meta
-// type Story = StoryObj<typeof meta>
-//
-// export const Primary: Story = {
-//   args: {
-//     variant: 'primary',
-//     children: 'Primary Button',
-//     disabled: false,
-//   },
-// }
+import { TextField } from './'
+
+const meta = {
+  title: 'Components/TextField',
+  component: TextField,
+  tags: ['autodocs'],
+  argTypes: {
+    // variant: {
+    //   options: ['primary', 'secondary', 'tertiary', 'link'],
+    //   control: { type: 'radio' },
+    // },
+  },
+} satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    // variant: 'primary',
+    // children: 'Primary Button',
+    // disabled: false,
+  },
+}
