b852a5671473dae5b32119622f48eb51aa52eb4e
Anna Krasilnikova <akr6363@mail.ru>
2023-07-24 22:37:32 +0300

    -import styles in preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,15 @@
 import type { Preview } from '@storybook/react'
-
+import '../src/styles/index.scss'
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/,
+        color: /(background|color)$/i,
+        date: /Date$/,
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
