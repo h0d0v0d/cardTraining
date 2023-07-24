0955ff1f3fa8cbd257dce8de5323a59ad994b742
Faukkirik <kote_33419@mail.ru>
2023-07-24 22:00:10 +0300

    button scss

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,16 +1,17 @@
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType } from 'react'
 
 import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
-  children: ReactNode
   variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
   fullWidth?: boolean
   className?: string
 } & ComponentPropsWithoutRef<T>
 
-export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
 
   return (
