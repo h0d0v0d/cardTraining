7dfdc7330e63f35e81d3a3881b02171365b4b9ef
Егор Серафонтов <pixaretyiypop@gmail.com>
2023-07-26 01:20:24 +0300

    small refactor

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,19 +1,21 @@
-import { FC, ReactNode } from 'react'
-import s from './card.module.scss'
+import { FC, PropsWithChildren } from 'react'
+
 import { clsx } from 'clsx'
+
 import { Typography } from '../typography'
 
+import s from './card.module.scss'
+
 type CardProps = {
-  children?: ReactNode
   className?: string
   title: string
 }
 
-export const Card: FC<CardProps> = ({ children, className, title }) => {
+export const Card: FC<PropsWithChildren<CardProps>> = ({ children, className, title }) => {
   return (
     <div className={clsx(s.root, className)}>
       <div className={s.title}>
-        <Typography variant={'large'}>{title}</Typography>
+        <Typography variant={'Large'}>{title}</Typography>
       </div>
       <div className={s.content}>{children}</div>
     </div>
