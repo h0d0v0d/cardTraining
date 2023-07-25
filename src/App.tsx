2dd643bb0122abd0efb2f9950edbc0606f0cfdf4
Faukkirik <kote_33419@mail.ru>
2023-07-25 22:54:56 +0300

    button scss (primary, secondary, tertiary, link) ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,4 @@
-import { TextField } from './components/ui/text-field'
-import { Button } from './components/ui/button'
 
 export function App() {
-  return (
-    <div>
-      <div>f</div>
-      <Button variant={'primary'} >
-        Hello
-      </Button>
-      <Button variant={'link'} >
-        Hello
-      </Button>
-      <Button variant={'tertiary'} >
-        Hello
-      </Button>
-      <Button variant={'secondary'} >
-        Hello
-      </Button>
-      <TextField />
-    </div>
-  )
+  return <div>Hello</div>
 }
