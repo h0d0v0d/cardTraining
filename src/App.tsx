6dda86d4a32569bff82646039a94099d83d84140
Faukkirik <kote_33419@mail.ru>
2023-07-29 14:48:18 +0300

    checkbox test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react'
+
+import { Button } from './components/ui/button'
+import { Checkbox } from './components/ui/checkbox'
 import { Header } from './components/ui/header'
 
 export function App() {
+  const [check, setCheck] = useState(false)
+  const handleChecked = () => {
+    setCheck(!check)
+  }
+
   return (
     <div>
       <Header isLogin={false} />
+      <div>aaa</div>
+      <Button>a</Button>
+      <Checkbox checked={check} onChange={handleChecked} label={'hi'} />
     </div>
   )
 }
