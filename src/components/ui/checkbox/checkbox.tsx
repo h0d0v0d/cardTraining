db5e616685415cccc5c278dd105641abff5b16bc
Faukkirik <kote_33419@mail.ru>
2023-07-29 17:53:11 +0300

    checkbox tests

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,22 +1,15 @@
-import { FC } from 'react'
-
-import * as CheckboxRadix from '@radix-ui/react-checkbox'
-import * as LabelRadix from '@radix-ui/react-label'
-import { clsx } from 'clsx'
-
 import s from './checkbox.module.scss'
-import Check from './Property 1=Default Selected.svg'
+import { Check } from './Property 1=Default Selected.svg'
 
 export type CheckboxProps = {
-  checked: boolean
-  onChange: (checked: boolean) => void
-  disabled?: boolean
-  required?: boolean
+  className?: string
+  id: string
   label?: string
-  id?: string
+  isChecked?: boolean
+  onChange?: (event: any) => void
 }
 
-export const Checkbox: FC<CheckboxProps> = ({
+export const Checkbox: React.FC<CheckboxProps> = ({
   checked,
   onChange,
   disabled,
@@ -24,35 +17,11 @@ export const Checkbox: FC<CheckboxProps> = ({
   label,
   id,
 }) => {
-  const classNames = {
-    container: s.container,
-    buttonWrapper: clsx(s.buttonWrapper, disabled && s.disabled),
-    root: s.root,
-    indicator: s.indicator,
-    label: clsx(s.label, disabled && s.disabled),
   }
-
   return (
-    <div className={classNames.container}>
-      <LabelRadix.Root className={classNames.label}>
-        <div className={classNames.buttonWrapper}>
-          <CheckboxRadix.Root
-            className={classNames.root}
-            checked={checked}
-            onCheckedChange={onChange}
-            disabled={disabled}
-            required={required}
-            id={id}
-          >
-            {checked && (
-              <CheckboxRadix.Indicator className={classNames.indicator} asChild forceMount>
-                <Check />
-              </CheckboxRadix.Indicator>
-            )}
-          </CheckboxRadix.Root>
-        </div>
-        {label}
-      </LabelRadix.Root>
-    </div>
+    <label htmlFor={id} className={"checkbox"}>
+      <span className={"checkbox-inner"}></span>
+      <Check />
+    </label>
   )
 }
