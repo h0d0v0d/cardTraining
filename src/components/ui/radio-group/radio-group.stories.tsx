d8638c9cc9520cef9f1db80949fac62a813309db
Anna Krasilnikova <akr6363@mail.ru>
2023-08-01 18:12:03 +0300

    create radio-group component

---
+++ src/components/ui/radio-group/radio-group.stories.tsx
@@ -0,0 +1,39 @@
+import { action } from '@storybook/addon-actions'
+import type { Meta } from '@storybook/react'
+import { StoryObj } from '@storybook/react'
+
+import {
+  RadioGroup,
+  RadioGroupProps,
+  RadioItemType,
+} from '@/components/ui/radio-group/radio-group.tsx'
+
+const meta = {
+  title: 'Components/RadioGroup',
+  component: RadioGroup,
+  tags: ['autodocs'],
+} satisfies Meta<typeof RadioGroup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const items: RadioItemType[] = [
+  { label: 'RadioGroup1', value: 'rg1' },
+  { label: 'RadioGroup2', value: 'rg2' },
+  { label: 'RadioGroup3', value: 'rg3' },
+  { label: 'RadioGroup4', value: 'rg4' },
+  { label: 'RadioGroup5', value: 'rg5' },
+]
+
+export const Primary: Story = {
+  render: (args: RadioGroupProps) => {
+    return (
+      <RadioGroup items={args.items} defaultValue={args.defaultValue} onChange={args.onChange} />
+    )
+  },
+  args: {
+    items,
+    defaultValue: 'rg1',
+    onChange: action('value changed'),
+  },
+}
