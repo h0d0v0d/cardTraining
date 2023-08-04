2b1acd20f6986387254b2bd998157d78f9cbda69
Faukkirik <kote_33419@mail.ru>
2023-08-04 14:46:46 +0300

    button (preview)

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,6 +3,7 @@ import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 import s from './button.module.scss'
 
 import { Typography } from '@/components/ui'
+import { Variant } from '@/components/ui/typography'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
@@ -10,14 +11,26 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
   fullWidth?: boolean
   className?: string
+  variantTypography?: Variant
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
-  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+  const {
+    variant = 'primary',
+    fullWidth,
+    className,
+    as: Component = 'button',
+    variantTypography = 'Subtitle_2',
+    children,
+    ...rest
+  } = props
+  const buttonClass = `${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`
 
   return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest}>
-      <Typography>{props.children || variant}</Typography>
+    <Component className={buttonClass} {...rest}>
+      <Typography variant={variantTypography} color={buttonClass}>
+        {children}
+      </Typography>
     </Component>
   )
 }
