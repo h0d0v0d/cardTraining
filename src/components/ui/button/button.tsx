836ed59cbddb810fff1136653cbcd2bcbf1f80bd
Faukkirik <kote_33419@mail.ru>
2023-08-04 13:52:56 +0300

    fix button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,11 +1,23 @@
-export const Button = () => {
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import s from './button.module.scss'
+
+import { Typography } from '@/components/ui'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  children: ReactNode
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  fullWidth?: boolean
+  className?: string
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+
   return (
-    <button
-      onClick={() => {
-        alert('click')
-      }}
-    >
-      click
-    </button>
+    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest}>
+      <Typography>{props.children || variant}</Typography>
+    </Component>
   )
 }
