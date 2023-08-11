a6f0751169cd4863d28aba30e9cf6081e3e0987c
Егор Серафонтов <pixaretyiypop@gmail.com>
2023-08-11 17:27:36 +0300

    login form v1

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,3 +1,29 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui'
+
+const LoginSheme = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+  rememberme: z.boolean().optional().default(false),
+})
+
+type LoginShemeType = z.infer<typeof LoginSheme>
+
 export const LoginForm = () => {
-  return <div></div>
+  const { register, handleSubmit } = useForm<LoginShemeType>({
+    mode: 'onChange',
+  })
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register('email')} />
+      <input type="text" {...register('password')} />
+      <Button type="submit">Sign in</Button>
+    </form>
+  )
 }
