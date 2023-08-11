0cc843e88def17556539810318c36bf257a04abe
Егор Серафонтов <pixaretyiypop@gmail.com>
2023-08-11 21:48:48 +0300

    login form html structure is ready

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
 import { LoginSheme, LoginShemeType } from '@/common/shemes'
-import { Button, ControlledCheckbox, ControlledTextFiled } from '@/components/ui'
+import { Button, ControlledCheckbox, ControlledTextFiled, Typography } from '@/components/ui'
 
 export const LoginForm = () => {
   const {
@@ -35,8 +35,20 @@ export const LoginForm = () => {
         defaultValue={''}
         errorMessage={errors.password?.message}
       />
-      <ControlledCheckbox name="rememberMe" control={control} />
+      <ControlledCheckbox
+        name="rememberMe"
+        control={control}
+        defaultValue={true}
+        label="Remeber me"
+      />
+      <Typography variant="Body_2" as="a" href="#">
+        Forgot password?
+      </Typography>
       <Button type="submit">Sign in</Button>
+      <Typography variant="Body_2" color="var(--color-light-900)">
+        Dont have an account?
+      </Typography>
+      <Button variant="link">Sign up</Button>
     </form>
   )
 }
